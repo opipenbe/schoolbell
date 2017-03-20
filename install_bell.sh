c157d88e9c0377fdaf9b171400492493ba77bfb2
opipenbe <olari.pipenberg@gmail.com>
2017-03-20 15:57:16 +0200

    Update install_bell.sh

--- install_bell.sh
+++ install_bell.sh
@@ -6,7 +6,7 @@ read
 # Author: Olari Pipenberg
 
 # This script converts machine Debian-like machine for schoolbell application.
-# Prerequisites: Ubuntu 16.04+ or Debian 8+ like operating system, Internet access, atleast 2GB free memory (filesystem should be extended if you are using Raspberry PI).
+# Prerequisites: Ubuntu 16.04+ or Debian 8+ like operating system, Internet access, atleast 2GB free memory (filesystem must be extended if you are using Raspberry PI).
 # Please read carefully this script before executing. You will be warned!
 
 # TODO fail2ban, error checks
